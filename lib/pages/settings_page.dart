8cdb843ad6d1154006910d7076647ce67750e17e
jerichoi224 <jerichoi224@gmail.com>
2023-10-21 21:55:03 +0900

    refactor: settings row refactcor

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -30,6 +30,53 @@ class _SettingsState extends State<SettingsPage> {
     getInfo();
   }
 
+  Widget switchRow(double height, String text, Function onSwitch, bool value) {
+    return SizedBox(
+        height: height,
+        child: Padding(
+            padding: const EdgeInsets.fromLTRB(20, 0, 12, 0),
+            child: Row(
+              children: [
+                Text(
+                  text,
+                  style: mainTextStyle(TextStyleType.settingsMenu),
+                ),
+                const Spacer(),
+                Switch(
+                    activeColor: Colors.red,
+                    activeTrackColor: Colors.white,
+                    value: value,
+                    onChanged: (onChanged) {
+                      onSwitch(onChanged);
+                    }),
+              ],
+            )));
+  }
+
+  Widget inkwellRow(String text, Function onTap) {
+    return InkWell(
+        onTap: () async {
+          await onTap();
+        },
+        child: SizedBox(
+            height: rowHeight,
+            child: Padding(
+                padding: const EdgeInsets.fromLTRB(20, 4, 12, 4),
+                child: Row(
+                  children: [
+                    Text(
+                      text,
+                      style: mainTextStyle(TextStyleType.settingsMenu),
+                    ),
+                    const Spacer(),
+                    const Icon(
+                      Icons.arrow_forward_ios_rounded,
+                      size: 18,
+                    )
+                  ],
+                ))));
+  }
+
   void getInfo() async {
     initialIsTimeline = sharedPref.get(SharedPrefKeys.initialScreen) ==
         InitialScreen.timeline.tabIndex;
@@ -45,278 +92,163 @@ class _SettingsState extends State<SettingsPage> {
   }
 
   Widget uiInitialScreen() {
-    return SizedBox(
-        height: rowHeight,
-        child: Padding(
-            padding: const EdgeInsets.fromLTRB(20, 0, 12, 0),
-            child: Row(
-              children: [
-                Text(
-                  "Show Timeline on Start",
-                  style: mainTextStyle(TextStyleType.settingsMenu),
-                ),
-                const Spacer(),
-                Switch(
-                    activeColor: Colors.red,
-                    activeTrackColor: Colors.white,
-                    value: initialIsTimeline,
-                    onChanged: (onChanged) {
-                      sharedPref.set(
-                          SharedPrefKeys.initialScreen,
-                          onChanged
-                              ? InitialScreen.timeline.tabIndex
-                              : InitialScreen.albums.tabIndex);
-                      setState(() {
-                        initialIsTimeline = onChanged;
-                      });
-                    }),
-              ],
-            )));
+    return switchRow(rowHeight, 'Show Timeline on Start', (onChanged) {
+      sharedPref.set(
+          SharedPrefKeys.initialScreen,
+          onChanged
+              ? InitialScreen.timeline.tabIndex
+              : InitialScreen.albums.tabIndex);
+      setState(() {
+        initialIsTimeline = onChanged;
+      });
+    }, initialIsTimeline);
   }
 
   Widget pinButtons() {
-    return SizedBox(
-        height: rowHeight,
-        child: Padding(
-            padding: const EdgeInsets.fromLTRB(20, 0, 12, 0),
-            child: Row(
-              children: [
-                Text(
-                  "Pin Favorites/Videos to Top",
-                  style: mainTextStyle(TextStyleType.settingsMenu),
-                ),
-                const Spacer(),
-                Switch(
-                    activeColor: Colors.red,
-                    activeTrackColor: Colors.white,
-                    value: pinShortcuts,
-                    onChanged: (onChanged) {
-                      sharedPref.set(SharedPrefKeys.pinShortcuts, onChanged);
-                      eventController.sink
-                          .add(Event(EventType.settingsChanged, null));
+    return switchRow(rowHeight, "Pin Favorites/Videos to Top", (onChanged) {
+      sharedPref.set(SharedPrefKeys.pinShortcuts, onChanged);
+      eventController.sink.add(Event(EventType.settingsChanged, null));
 
-                      setState(() {
-                        pinShortcuts = onChanged;
-                      });
-                    }),
-              ],
-            )));
+      setState(() {
+        pinShortcuts = onChanged;
+      });
+    }, pinShortcuts);
   }
 
   Widget albumColumnCount() {
-    return SizedBox(
-        height: rowHeight,
-        child: Padding(
-            padding: const EdgeInsets.fromLTRB(20, 0, 12, 0),
-            child: Row(
-              children: [
-                Text(
-                  "Use 3 Columns For Albums",
-                  style: mainTextStyle(TextStyleType.settingsMenu),
-                ),
-                const Spacer(),
-                Switch(
-                    activeColor: Colors.red,
-                    activeTrackColor: Colors.white,
-                    value: numAlbumCol == 3,
-                    onChanged: (onChanged) {
-                      numAlbumCol = onChanged ? 3 : 2;
-                      sharedPref.set(
-                          SharedPrefKeys.albumsColCount, numAlbumCol);
-                      eventController.sink
-                          .add(Event(EventType.settingsChanged, null));
+    return switchRow(rowHeight, 'Use 3 Columns For Albums', (onChanged) {
+      setState(() {
+        numAlbumCol = onChanged ? 3 : 2;
+      });
 
-                      setState(() {});
-                    }),
-              ],
-            )));
+      sharedPref.set(SharedPrefKeys.albumsColCount, numAlbumCol);
+      eventController.sink.add(Event(EventType.settingsChanged, null));
+    }, numAlbumCol == 3);
   }
 
   Widget showHiddenAlbums() {
-    return InkWell(
-        onTap: () async {
-          await showDialog(
-              context: context,
-              builder: (BuildContext context) {
-                return AlertDialog(
-                    scrollable: true,
-                    contentPadding: const EdgeInsets.all(20),
-                    title: Text('Hidden Albums',
-                        style: mainTextStyle(TextStyleType.alertTitle)),
-                    shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(20.0))),
-                    content: SizedBox(
-                        width: double.maxFinite,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          mainAxisSize: MainAxisSize.min,
-                          children: <Widget>[
-                            Padding(
-                                padding:
-                                    const EdgeInsets.fromLTRB(20, 0, 20, 0),
-                                child: Consumer<AppStatus>(
-                                    builder: (context, appStatus, child) {
-                                  List<String> hiddenAlbums =
-                                      appStatus.hiddenAblums;
+    return inkwellRow("Show Hidden Albums", () async {
+      showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+                scrollable: true,
+                contentPadding: const EdgeInsets.all(20),
+                title: Text('Hidden Albums',
+                    style: mainTextStyle(TextStyleType.alertTitle)),
+                shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20.0))),
+                content: SizedBox(
+                    width: double.maxFinite,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Padding(
+                            padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                            child: Consumer<AppStatus>(
+                                builder: (context, appStatus, child) {
+                              List<String> hiddenAlbums =
+                                  appStatus.hiddenAblums;
 
-                                  if (hiddenAlbums.isEmpty) {
-                                    return Padding(
-                                        padding: const EdgeInsets.symmetric(
-                                            vertical: 10),
-                                        child: Text(
-                                          "You have no hidden albums.",
-                                          style: mainTextStyle(TextStyleType
-                                              .settingsPageDescription),
-                                        ));
-                                  }
+                              if (hiddenAlbums.isEmpty) {
+                                return Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        vertical: 10),
+                                    child: Text(
+                                      "You have no hidden albums.",
+                                      style: mainTextStyle(TextStyleType
+                                          .settingsPageDescription),
+                                    ));
+                              }
 
-                                  return Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: Provider.of<AlbumInfoList>(
-                                            context,
-                                            listen: false)
-                                        .albums
-                                        .where((album) => hiddenAlbums
-                                            .contains(album.pathEntity.id))
-                                        .map((album) => Row(
-                                              children: [
-                                                Text(
-                                                  album.pathEntity.name
-                                                      .toUpperCase(),
-                                                  style: mainTextStyle(TextStyleType
-                                                      .settingsPageDescription),
-                                                ),
-                                                const Spacer(),
-                                                IconButton(
-                                                    onPressed: () {
-                                                      appStatus
-                                                          .removeHiddenAlbum([
-                                                        album.pathEntity.id
-                                                      ]);
-                                                    },
-                                                    icon:
-                                                        const Icon(Icons.close))
-                                              ],
-                                            ))
-                                        .toList(),
-                                  );
-                                })),
-                            const SizedBox(
-                              height: 10,
-                            )
-                          ],
-                        )));
-              });
-        },
-        child: SizedBox(
-            height: rowHeight,
-            child: Padding(
-                padding: const EdgeInsets.fromLTRB(20, 4, 12, 4),
-                child: Row(
-                  children: [
-                    Text(
-                      "Show Hidden Albums",
-                      style: mainTextStyle(TextStyleType.settingsMenu),
-                    ),
-                    const Spacer(),
-                    const Icon(
-                      Icons.arrow_forward_ios_rounded,
-                      size: 18,
-                    )
-                  ],
-                ))));
+                              return Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: Provider.of<AlbumInfoList>(context,
+                                        listen: false)
+                                    .albums
+                                    .where((album) => hiddenAlbums
+                                        .contains(album.pathEntity.id))
+                                    .map((album) => Row(
+                                          children: [
+                                            Text(
+                                              album.pathEntity.name
+                                                  .toUpperCase(),
+                                              style: mainTextStyle(TextStyleType
+                                                  .settingsPageDescription),
+                                            ),
+                                            const Spacer(),
+                                            IconButton(
+                                                onPressed: () {
+                                                  appStatus.removeHiddenAlbum(
+                                                      [album.pathEntity.id]);
+                                                },
+                                                icon: const Icon(Icons.close))
+                                          ],
+                                        ))
+                                    .toList(),
+                              );
+                            })),
+                        const SizedBox(
+                          height: 10,
+                        )
+                      ],
+                    )));
+          });
+    });
   }
 
   Widget license() {
-    return InkWell(
-        onTap: () async {
-          final Uri url = Uri.parse('https://www.gnu.org/licenses/gpl-3.0.txt');
+    return inkwellRow("License", () async {
+      final Uri url = Uri.parse('https://www.gnu.org/licenses/gpl-3.0.txt');
 
-          if (!await launchUrl(url)) {
-            throw Exception('Could not launch $url');
-          }
-        },
-        child: SizedBox(
-            height: rowHeight,
-            child: Padding(
-                padding: const EdgeInsets.fromLTRB(20, 4, 12, 4),
-                child: Row(
-                  children: [
-                    Text(
-                      "License",
-                      style: mainTextStyle(TextStyleType.settingsMenu),
-                    ),
-                    const Spacer(),
-                    const Icon(
-                      Icons.arrow_forward_ios_rounded,
-                      size: 18,
-                    )
-                  ],
-                ))));
+      if (!await launchUrl(url)) {
+        throw Exception('Could not launch $url');
+      }
+    });
   }
 
   Widget credits() {
-    return InkWell(
-        onTap: () async {
-          await showDialog(
-              context: context,
-              builder: (BuildContext context) {
-                return SimpleDialog(
-                  title: Text('Credits',
-                      style: mainTextStyle(TextStyleType.alertTitle)),
-                  children: <Widget>[
-                    Padding(
-                        padding: const EdgeInsets.fromLTRB(20, 10, 20, 0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text("Develop",
-                                style: mainTextStyle(
-                                    TextStyleType.creditsCateogry)),
-                            Text("Daniel Choi",
+    return inkwellRow("Credits", () async {
+      await showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return SimpleDialog(
+              title: Text('Credits',
+                  style: mainTextStyle(TextStyleType.alertTitle)),
+              children: <Widget>[
+                Padding(
+                    padding: const EdgeInsets.fromLTRB(20, 10, 20, 0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Develop",
+                            style:
+                                mainTextStyle(TextStyleType.creditsCateogry)),
+                        Text("Daniel Choi",
+                            style: mainTextStyle(TextStyleType.creditsName)),
+                        const SizedBox(height: 10),
+                        Text("Design",
+                            style:
+                                mainTextStyle(TextStyleType.creditsCateogry)),
+                        Text("Alkid Shuli, Daniel Choi",
+                            style: mainTextStyle(TextStyleType.creditsName)),
+                        const SizedBox(height: 15),
+                        Center(
+                            child: DialogBottomButton(
+                                text: 'Close',
+                                onTap: () => {
+                                      if (Navigator.canPop(context))
+                                        {Navigator.pop(context)}
+                                    },
                                 style:
-                                    mainTextStyle(TextStyleType.creditsName)),
-                            const SizedBox(height: 10),
-                            Text("Design",
-                                style: mainTextStyle(
-                                    TextStyleType.creditsCateogry)),
-                            Text("Alkid Shuli, Daniel Choi",
-                                style:
-                                    mainTextStyle(TextStyleType.creditsName)),
-                            const SizedBox(height: 15),
-                            Center(
-                                child: DialogBottomButton(
-                                    text: 'Close',
-                                    onTap: () => {
-                                          if (Navigator.canPop(context))
-                                            {Navigator.pop(context)}
-                                        },
-                                    style: mainTextStyle(
-                                        TextStyleType.creditsClose)))
-                          ],
-                        ))
-                  ],
-                );
-              });
-        },
-        child: SizedBox(
-            height: rowHeight,
-            child: Padding(
-                padding: const EdgeInsets.fromLTRB(20, 4, 12, 4),
-                child: Row(
-                  children: [
-                    Text(
-                      "Credits",
-                      style: mainTextStyle(TextStyleType.settingsMenu),
-                    ),
-                    const Spacer(),
-                    const Icon(
-                      Icons.arrow_forward_ios_rounded,
-                      size: 18,
-                    )
-                  ],
-                ))));
+                                    mainTextStyle(TextStyleType.creditsClose)))
+                      ],
+                    ))
+              ],
+            );
+          });
+    });
   }
 
   Widget versionInfo() {
